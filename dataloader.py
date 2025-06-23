3a32274de4d3cc976627f398b77e7453d32c09a6
Arthur-St-06 <arthur.stupa@gmail.com>
2025-06-22 23:21:28 -0700

    Added kuberflow

--- dataloader.py
+++ dataloader.py
@@ -1,7 +1,7 @@
 import torch
 
 def get_dataloader(batch_size, rank, world_size):
-    dataset = torch.load("./data/mnist_train.pt")  # Load saved dataset
+    dataset = torch.load("./data/mnist_train.pt")
 
     sampler = torch.utils.data.distributed.DistributedSampler(
         dataset, num_replicas=world_size, rank=rank
