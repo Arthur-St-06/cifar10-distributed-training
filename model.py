78654389077a8f71b9eb0e158c0fe07324d0e568
Arthur-St-06 <arthur.stupa@gmail.com>
2025-06-24 16:44:20 +0000

    Added new dataset and updated model

--- model.py
+++ model.py
@@ -4,10 +4,18 @@ class SimpleModel(nn.Module):
     def __init__(self):
         super().__init__()
         self.net = nn.Sequential(
-            nn.Flatten(),
-            nn.Linear(28 * 28, 256),
+            nn.Conv2d(3, 32, kernel_size=3, padding=1),  # output: 32×32×32
             nn.ReLU(),
-            nn.Linear(256, 10)
+            nn.MaxPool2d(2),                             # → 32×16×16
+
+            nn.Conv2d(32, 64, kernel_size=3, padding=1), # → 64×16×16
+            nn.ReLU(),
+            nn.MaxPool2d(2),                             # → 64×8×8
+
+            nn.Flatten(),                                # → 64*8*8 = 4096
+            nn.Linear(4096, 128),
+            nn.ReLU(),
+            nn.Linear(128, 10)
         )
 
     def forward(self, x):
