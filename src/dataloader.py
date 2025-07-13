793c7eff7d54c70dc14d260beb523d4d48e3f18a
Arthur-St-06 <arthur.stupa@gmail.com>
2025-07-13 11:18:10 -0700

    Cleaned up + added local running support

--- src/dataloader.py
+++ src/dataloader.py
@@ -2,7 +2,7 @@ import os
 import torch
 
 def get_dataloader(batch_size, rank, world_size):
-    dataset_path = os.getenv("DATA_PATH1", "./data")
+    dataset_path = os.getenv("DATA_PATH", "./data")
     full_path = os.path.join(dataset_path, "cifar10_train.pt")
 
     dataset = torch.load(full_path)
